95788be086265c3c4b32b74d57913a157919e108
allen0099 <allen0099@sudo.host>
2022-12-15 17:49:18 +0800

    ✨ Update pack script

--- pack.sh
+++ pack.sh
@@ -1,6 +1,8 @@
 helm package charts/$1/$2
 mv $1-$2.tgz ../devops-charts-pack-and-index/
 cd ../devops-charts-pack-and-index
+git pull
 helm repo index .
 git add .
+git commit -am "✨ Update $1 $2"
 git push
